92cf6bfed31c4fe4c1fc4ea62f31d7931a7126b7
Vinay <vinay.py@gmail.com>
2021-03-13 13:12:55 -0500

    First Commit

---
+++ src/main/java/com/example/Cryptozoology/CryptozoologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cryptozoology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptozoologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptozoologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cryptozoology/CryptozoologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cryptozoology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozoologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
