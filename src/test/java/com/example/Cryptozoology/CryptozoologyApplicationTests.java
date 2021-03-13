a2e1615182f6869f7f66a3cbd42ebc62b66430ad
Vinay <vinay.py@gmail.com>
2021-03-13 13:45:13 -0500

    Red commit

--- src/test/java/com/example/Cryptozoology/CryptozoologyApplicationTests.java
+++ src/test/java/com/example/Cryptozoology/CryptozoologyApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.Cryptozoology;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.awt.*;
+
 @SpringBootTest
 class CryptozoologyApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void addAnimalsTest() {
+//		mockMVC.perform(post("/add")).content("").contentType(PageAttributes.MediaType.APPLICATION_JSON)
+
 	}
 
 }
